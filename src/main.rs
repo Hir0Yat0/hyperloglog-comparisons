9646fcb9056f8c045db2e02ca86f788eca86cb27
Hir0Yat0 <xToppyThailandx@gmail.com>
2025-04-03 09:17:11 +0700

    hll++

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod vanilla;
 mod universalhash;
 mod benchmark;
+mod hllpp;
+
+use vlq::{ReadVlqExt, WriteVlqExt};
 
 fn main() {
     println!("Hello, world!");
     // benchmark::vanilla::run_benchmark_speed();
-    benchmark::vanilla::run_benchmark_accuracy();
+    // benchmark::vanilla::run_benchmark_accuracy();
 }
